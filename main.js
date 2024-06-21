aae99cd5dcacd5424c932bf0ff51c896c1e2decb
blakehulett7 <blake.hulett7@gmail.com>
2024-06-20 21:14:43 -0500

    We can now accept arguments from the command line!!

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Christ is King!')
+import process from 'node:process'
+
+function main() {
+  const input = process.argv
+  const clArgs = input.slice(2, input.length)
+  if (clArgs.length != 1) {
+    console.log('Error: Incorrect number of arguments')
+    return
+  }
+  console.log(`Crawler starting at ${clArgs[0]}...`)
+}
+
+main()
